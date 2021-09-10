8ce15032ed70cfb3b61d37caa04904812790b6cc
Simar <72882316+IcyAltair@users.noreply.github.com>
2021-09-10 11:08:38 +0300

    Delete TAD directory

--- TAD/headers/graphics.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-int drawEightPoints(int r, int* x, int* y, int it, int cr);
-int drawCirclePoints(int r, int* x, int* y,int it, int cr);
-void drawPoint(int* x, int* y,int size);
-void example();
--- TAD/source/tools.c
+++
@@ -1,9 +0,0 @@
-#include "source.h"
-
-void fullScreen()
-{
-	keybd_event(VK_MENU, 0x38, 0, 0);
-	keybd_event(VK_RETURN, 0x1c, 0, 0);
-	keybd_event(VK_RETURN, 0x1c, KEYEVENTF_KEYUP, 0);
-	keybd_event(VK_MENU, 0x38, KEYEVENTF_KEYUP, 0);
-}
