1fa4e2330d8c4df3cdc6f375919faa9fc4d05bec
Simar <72882316+IcyAltair@users.noreply.github.com>
2021-09-10 11:09:47 +0300

    Add files via upload

---
+++ TDA/headers/graphics.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int drawEightPoints(int r, int* x, int* y, int it, int cr);
+int drawCirclePoints(int r, int* x, int* y,int it, int cr);
+void drawPoint(int* x, int* y,int size);
+void example();
---
+++ TDA/headers/source.h
@@ -0,0 +1,19 @@
+#pragma once
+//#include "generator.h"
+//#include "graphics.h"
+//#include "tools.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <windows.h>
+#include <conio.h>
+#include <math.h>
+
+#define DEBUG 0
+#define N 1000
+#define E 10
+#define STEP 1
+#define PI acos(-1)
