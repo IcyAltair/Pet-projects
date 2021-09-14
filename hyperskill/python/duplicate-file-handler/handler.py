a6283ae475f8517043771c42e5589b7b359ad237
Simar <72882316+IcyAltair@users.noreply.github.com>
2021-09-14 14:46:41 +0300

    Update handler.py

--- hyperskill/python/duplicate-file-handler/handler.py
+++ hyperskill/python/duplicate-file-handler/handler.py
@@ -1,1 +1,21 @@
+import argparse
 
+
+class Duplicator:
+    def __init__(self):
+        self.directory = ""
+
+    def parse(self):
+        parser = argparse.ArgumentParser(description="Building file tree")
+        parser.add_argument("dir", type=str, help="Main directory to build a tree of folders and files")
+        args = parser.parse_args()
+        self.directory = args.dir
+
+
+def main():
+    file_handler = Duplicator()
+    file_handler.parse()
+
+
+if __name__ == "__main__":
+    main()
