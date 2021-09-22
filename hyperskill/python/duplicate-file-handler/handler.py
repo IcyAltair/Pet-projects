461bb0283628c0929a906f736a16c12dbcf5f5b3
Simar <72882316+IcyAltair@users.noreply.github.com>
2021-09-22 22:40:09 +0300

    Update handler.py

--- hyperskill/python/duplicate-file-handler/handler.py
+++ hyperskill/python/duplicate-file-handler/handler.py
@@ -7,8 +7,14 @@ class Duplicator:
     def __init__(self):
         self.directory = ""
         self.data = []
+        self.ext = ""
+        self.way = 0
+        self.paths = []
+        self.file = {"path": "", "ext": "", "size": ""}
+        self.library = []
 
     def parse(self):
+        ways = ("1", "2", "")
         if len(sys.argv) < 2:
             print("Directory is not specified")
             sys.exit(1)
@@ -16,15 +22,33 @@ class Duplicator:
         parser.add_argument("dir", type=str, help="Main directory to build a tree of folders and files")
         args = parser.parse_args()
         self.directory = args.dir
+        self.ext = input("Enter file format:\n")
+        print("\nSize sorting options:")
+        print("1. Descending")
+        print("2. Ascending\n")
+        self.way = input("Enter a sorting option:\n")
+        while self.way not in ways:
+            print("\nWrong option\n")
+            self.way = input("Enter a sorting option:\n")
 
     def build_tree(self):
         for i in os.walk(self.directory):
             self.data.append(i)
 
-    def print_tree(self):
+    def print_tree(self, show=False):
         for address, dirs, files in self.data:
             for file in files:
-                print(address + '/' + file)
+                path = address + "\\" + file
+                if show:
+                    print(path)
+                self.paths.append(path)
+
+    def get_file_info(self):
+        for path in self.paths:
+            self.file.update({"path": path, "ext": os.path.splitext(path)[1][1:], "size": os.path.getsize(path)})
+            self.library.append(self.file)
+            self.file = {}
+        print(self.library)
 
 
 def main():
@@ -32,6 +56,7 @@ def main():
     file_handler.parse()
     file_handler.build_tree()
     file_handler.print_tree()
+    file_handler.get_file_info()
 
 
 if __name__ == "__main__":
